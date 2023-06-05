6ec93a9f71a6564e0dd56b81b047ace60cd8219d
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-05 10:19:58 +0300

    Done with Assignment

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,38 @@
 class Pokemon
+    require_relative "../db/db_connection"
+
+class Pokemon
+  attr_accessor :id, :name, :type
+
+  def initialize(id:, name:, type:)
+    @id = id
+    @name = name
+    @type = type
+  end
+
+  def self.save(name, type)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+
+    DB.execute(sql, name, type)
+  end
+
+  def self.find(id)
+    sql = <<-SQL
+      SELECT *
+      FROM pokemon
+      WHERE id = ?
+    SQL
+
+    result = DB.execute(sql, id)[0]
+    if result
+      Pokemon.new(id: result[0], name: result[1], type: result[2])
+    else
+      nil
+    end
+  end
+end
+
 end
